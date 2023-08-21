e424ecedb2710e82e14c4faa14a5a890b0bebf68
dalba <davidain03@gmail.com>
2023-08-21 17:50:11 -0600

    Navigation improved

--- src/features/AppBar/AppBar.tsx
+++ src/features/AppBar/AppBar.tsx
@@ -4,10 +4,15 @@ import AppBarMaterial from "@mui/material/AppBar";
 import { AccountCircle } from "@mui/icons-material";
 import MenuIcon from "@mui/icons-material/Menu";
 import Drawer from "./Drawer";
+import { Outlet } from "react-router-dom";
 
 const AppBar = () => {
   const [openDrawer, setOpenDrawer] = useState(false);
-  const navItems = ["Home", "Selector", ""];
+  const navItems = [
+    { display: "Home", route: "" },
+    { display: "Selector", route: "selector" },
+    { display: "About", route: "about" },
+  ];
 
   const handleOpenMenu = (e: React.MouseEvent<HTMLElement>) => {
     e.stopPropagation();
@@ -17,30 +22,39 @@ const AppBar = () => {
     setOpenDrawer(false);
   };
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBarMaterial>
-        <Toolbar>
-          <Drawer
-            handleToggle={handleCloseMenu}
-            isOpen={openDrawer}
-            navItems={navItems}
-          />
-          <Box sx={{ display: { xs: "flex", md: "none" } }}>
-            <MenuItem onClick={handleOpenMenu}>
-              <IconButton size="large" edge="end" aria-label="user info">
-                <MenuIcon />
-              </IconButton>
-            </MenuItem>
+    <div>
+      <header>
+        <nav>
+          <Box sx={{ flexGrow: 1 }}>
+            <AppBarMaterial>
+              <Toolbar>
+                <Drawer
+                  handleToggle={handleCloseMenu}
+                  isOpen={openDrawer}
+                  navItems={navItems}
+                />
+                <Box sx={{ display: { xs: "flex", md: "none" } }}>
+                  <MenuItem onClick={handleOpenMenu}>
+                    <IconButton size="large" edge="end" aria-label="user info">
+                      <MenuIcon />
+                    </IconButton>
+                  </MenuItem>
+                </Box>
+                <Box sx={{ flexGrow: 1 }} />
+                <Box sx={{ display: { xs: "flex", md: "none" } }}>
+                  <IconButton size="large" edge="end" aria-label="user info">
+                    <AccountCircle />
+                  </IconButton>
+                </Box>
+              </Toolbar>
+            </AppBarMaterial>
           </Box>
-          <Box sx={{ flexGrow: 1 }} />
-          <Box sx={{ display: { xs: "flex", md: "none" } }}>
-            <IconButton size="large" edge="end" aria-label="user info">
-              <AccountCircle />
-            </IconButton>
-          </Box>
-        </Toolbar>
-      </AppBarMaterial>
-    </Box>
+        </nav>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </div>
   );
 };
 
